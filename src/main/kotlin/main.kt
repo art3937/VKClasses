0b93224837c8435a3835cb51849d43376435951a
Artiom test <art3937@gmail.com>
2024-11-29 00:44:54 +0300

    Null

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,20 @@
-import kotlin.random.Random
-
 fun main() {
-    val reposts = Reposts(10, false)
+    val reposts: Reposts? = null
     val post = Post(
-        0, 0, 0, 0, "text", false, 0, false, reposts
+        0, 0, 0, 0, "text", false, 0, false,reposts
     )
-    println(WallService.add(post))
+    println(WallService.add(post).reposts)
     println(WallService.update(post))
 }
 
-data class Post(
+ data class Post(
     var id: Int, val fromId: Int, //Идентификатор автора записи (от чьего имени опубликована запись).
     val createdBy: Int, //Идентификатор администратора, который опубликовал запись
     val replyOwnerId: Int, //Идентификатор владельца записи, в ответ на которую была оставлена текущая.
     val text: String, val friendsOnly: Boolean, //1, если запись была создана с опцией «Только для друзей».
     val replyPostId: Int, //Идентификатор записи, в ответ на которую была оставлена текущая
     val canDelete: Boolean, //Информация о том, может ли текущий пользователь удалить запись (1 — может, 0 — не может)
-    val reposts: Reposts
+    val reposts: Reposts?
 )
 
 object WallService {
@@ -31,9 +29,10 @@ object WallService {
 
     fun update(post: Post): Boolean {
         var result = false
+        val copyPost = post.copy()
         for ((index, anotherPost) in posts.withIndex()) {
-            if (post.id == anotherPost.id) {
-                posts[index] = post
+            if (copyPost.id == anotherPost.id) {
+                posts[index] = copyPost
                 result = true
             }
         }
@@ -42,13 +41,14 @@ object WallService {
 
     fun clear() {
         posts = emptyArray()
+        count = 0
         // также здесь нужно сбросить счетчик для id постов, если он у вас используется
     }
 }
 
 class Reposts(
     val count: Int, //число пользователей, скопировавших запись;
-    val userReposted: Boolean  //наличие репоста от текущего пользователя (1 — есть, 0 — нет).
-) {
+    val userReposted: Boolean)  //наличие репоста от текущего пользователя (1 — есть, 0 — нет)
+    {
 //Логика
 }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -1,7 +1,6 @@
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
-import kotlin.random.Random
 
 class WallServiceTest {
 
@@ -12,8 +11,6 @@ class WallServiceTest {
             2, false
         )
     )
-    private var posts = emptyArray<Post>()
-    private var count = 0
 
     @Before
     fun clearBeforeTest() {
@@ -23,18 +20,20 @@ class WallServiceTest {
     @Test
     fun add() {
         val copyPost = post
-      posts += WallService.add(copyPost)
-        assertTrue(posts.last().id>0)
+        val result: Post = WallService.add(copyPost)
+        assertTrue(result.id>0)
 
     }
 
     @Test
     fun update() {
-        assertTrue(WallService.update(WallService.add(post)))
+        val copyPost = post.copy()
+        assertTrue(WallService.update(WallService.add(copyPost)))
     }
 
     @Test
     fun updateFalse() {
-        assertFalse(WallService.update(post))
+        val copyPost = post.copy()
+        assertFalse(WallService.update(copyPost))
     }
 }
